50b4c104b24be4e3120e1223cc36e0e38bbefb44
Ali Mamedov <ali@mamedov.xyz>
2019-02-26 14:14:10 +0400

    Disabled speaking. I saw that at some computers it is not working properly.

--- classes/Commands.py
+++ classes/Commands.py
@@ -237,7 +237,7 @@ class SearchExploreButton(AbstractMethods.ProcessHandler):
             CheckAntibot().do_work()
         else:
             print('Continue exploration...')
-            Speak().speak('Continue exploration!')
+            # Speak().speak('Continue exploration!')
         self.next()
 
 
